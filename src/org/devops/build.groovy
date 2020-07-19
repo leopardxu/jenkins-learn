27e0694bb6e35788a86ffcd64d2abdbd9b5a390f
sealandsigh <zhujiajunzhu@qq.com>
2020-07-19 19:09:58 +0800

    update cj jenkins gradle

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -9,6 +9,13 @@ def Build(buildType,buildShell){
     println("当前选择的构建类型为${buildType}")
     buildHome = tool buildTools[buildType]
 
-    sh "${buildHome}/bin/${buildType} ${buildShell}"
-
+    if ("${buildType}" == "npm") {
+        sh """
+           export NODE_HOME=${buildhome}
+           export PATH=\$NODE_HOME/bin:\$PATH 
+           ${buildhome}/bin/${buildType} ${buildShell}
+           """
+    } else {
+        sh "${buildHome}/bin/${buildType} ${buildShell}"
+    }
 }
