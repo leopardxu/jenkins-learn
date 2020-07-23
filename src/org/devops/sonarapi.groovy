a8db73ae94a68c5f8c4354ca5092a3d6fa55e434
sealandsigh <zhujiajunzhu@qq.com>
2020-07-23 20:31:05 +0800

    add sonarapi.groovy

---
+++ src/org/devops/sonarapi.groovy
@@ -0,0 +1,30 @@
+package org.devops
+
+
+//封装HTTP
+
+def HttpReq(reqType,reqUrl,reqBody){
+    def sonarServer = "http://192.168.1.200:30090/api"
+   
+    result = httpRequest authentication: 'sonar-admin-user',
+            httpMode: reqType, 
+            contentType: "APPLICATION_JSON",
+            consoleLogResponseBody: true,
+            ignoreSslErrors: true, 
+            requestBody: reqBody,
+            url: "${sonarServer}/${reqUrl}"
+            //quiet: true
+    
+    return result
+}
+
+//获取Sonar质量阈状态
+def GetProjectStatus(projectName){
+    apiUrl = "project_branches/list?project=${projectName}"
+    response = HttpReq("GET",apiUrl,'')
+
+    response = readJSON text: """${response.content}"""
+
+    println(response)
+    
+    return response
